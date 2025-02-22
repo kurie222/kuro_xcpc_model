d35b11d891dad47ae791dbacc2322e7d6a195a9e
kurie222 <visuarte@163.com>
2025-02-22 17:53:39 +0800

    模版化了matrix类

--- Math/matrix.h
+++ Math/matrix.h
@@ -1,14 +1,29 @@
+/*
+    2025/2/22更新:
+    使用模版重写了这个远古矩阵类，使其支持用二维vector直接生成
+    注意，普通的快速幂使用的幺元是1，矩阵需要使用单位矩阵
+    所以矩阵快速幂在使用时需调用矩阵的qpow成员函数代替泛型快速幂！！！
+    尤其是在联合mint使用时
+*/
+
+template <typename T>
 struct matrix
 {
-    vector<vector<int>> data;
-    int row, column; // 行，列
-    long long MOD = __INT64_MAX__;
+    vector<vector<T>> data;
+    int row, column; // 行，列 :1-index
+    matrix(vector<vector<T>> vec)
+    {
+        data = vec;
+        row = vec.size() - 1;
+        column = vec[1].size() - 1;
+    }
     matrix(int n) // 构造n行n列矩阵
     {
         data.resize(n + 1);
         for (int i = 1; i <= n; i++)
         {
             data[i].resize(n + 1);
+            data[i][i] = 1;
         }
         row = n;
         column = n;
@@ -23,78 +38,72 @@ struct matrix
         row = n;
         column = m;
     }
-    void setmod(int modd)
+    matrix<T> operator-(const matrix<T> &temp) const
     {
-        MOD=modd;
-    }
-    matrix operator-(const matrix &T) const
-    {
-        if (this->column != T.column || this->row != T.row)
+        if (this->column != temp.column || this->row != temp.row)
         {
             cerr << "minus error!" << endl;
-            return matrix(0);
+            return matrix<T>(0);
         }
-        matrix res(*this);
+        matrix<T> res(*this);
         for (int i = 1; i <= row; ++i)
             for (int j = 1; j <= column; ++j)
             {
-                res.data[i][j] -= T.data[i][j];
-                res.data[i][j] = (res.data[i][j] + MOD) % MOD;
+                res.data[i][j] -= temp.data[i][j];
+                res.data[i][j] = (res.data[i][j]);
             }
         return res;
     }
 
-    matrix operator+(const matrix &T) const
+    matrix<T> operator+(const matrix<T> &temp) const
     {
-        if (this->column != T.column || this->row != T.row)
+        if (this->column != temp.column || this->row != temp.row)
         {
             cerr << "add error!" << endl;
-            return matrix(0);
+            return matrix<T>(0);
         }
-        matrix res(*this);
+        matrix<T> res(*this);
         for (int i = 1; i <= row; ++i)
             for (int j = 1; j <= column; ++j)
             {
-                res.data[i][j] += T.data[i][j];
-                res.data[i][j] %= MOD;
+                res.data[i][j] += temp.data[i][j];
             }
         return res;
     }
 
-    matrix operator*(const matrix &T) const
+    matrix<T> operator*(const matrix<T> &temp) const
     {
-        if (this->column != T.row)
+        if (this->column != temp.row)
         {
             cerr << "mul error" << endl;
-            return matrix(0);
+            return matrix<T>(0);
         }
-        matrix res(row, T.column);
-        int r;
+        matrix<T> res(row, temp.column);
+        mint r;
         for (int i = 1; i <= res.row; ++i)
             for (int k = 1; k <= this->column; ++k)
             {
                 r = data[i][k];
                 for (int j = 1; j <= res.column; ++j)
-                    res.data[i][j] += T.data[k][j] * r, res.data[i][j] %= MOD;
+                    res.data[i][j] += temp.data[k][j] * r;
             }
         return res;
     }
-
-    matrix operator^(long long x) const
+    matrix<T> qpow(int64_t x)
     {
-        if (column != row)
+        if (this->column != this->row)
         {
             cerr << "pow error" << Endl;
-            return matrix(0);
+            return matrix<T>(0);
         }
         if (x == 1)
             return *this;
-        matrix res(column), bas(column);
-        for (int i = 1; i <= column; ++i)
+        matrix<T> res(this->column), bas(this->column);
+        for (int i = 1; i <= this->column; ++i)
             res.data[i][i] = 1;
-        for (int i = 1; i <= column; ++i)
-            for (int j = 1; j <= column; ++j)
-                bas.data[i][j] = data[i][j] % MOD;
+        for (int i = 1; i <= this->column; ++i)
+            for (int j = 1; j <= this->column; ++j)
+                bas.data[i][j] = this->data[i][j];
         while (x)
         {
             if (x & 1)
